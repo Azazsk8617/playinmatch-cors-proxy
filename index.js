048231e2930eebc48b402dd07916785297a4a1b7
Azazsk8617 <azazsk384@gmail.com>
2025-06-26 02:07:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,18 +9,16 @@ app.use(express.json());
 
 app.post('/register', async (req, res) => {
   try {
-    const token = req.headers['x-csrf-token'];
     const form = new URLSearchParams();
     form.append('phone', req.body.mobile);
     form.append('username', req.body.username);
     form.append('email', req.body.email);
     form.append('password', req.body.password);
-    form.append('password_confirmation', req.body.mobile);
+    form.append('password_confirmation', req.body.password);
 
     const response = await axios.post('https://playinmatch.com/api2/v2/register', form, {
       headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'X-CSRF-TOKEN': token
+        'Content-Type': 'application/x-www-form-urlencoded'
       }
     });
 
